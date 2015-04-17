7d7eae0478309f2335e008fa0b561b3e79a4fb71
Alex Howard <alexhoward007@hotmail.com>
2015-04-18 00:05:16 +0100

    created guestlist app

---
+++ blog-app/routes/users.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET userlist */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  db.collection('userlist').find().toArray(function(err, items){
+    res.json(items);
+  });
+});
+
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  db.collection('userlist').insert(req.body, function(err, result){
+      res.send(
+        (err === null) ? { msg: '' } : { msg: err }
+      );
+  });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id',function(req,res){
+  var db = req.db;
+  var userToDelete = req.params.id;
+  db.collection('userlist').removeById(userToDelete, function(err, result){
+    res.send((result === 1) ? {msg:''}:{msg:'error: '+err});
+  });
+});
+
+/*
+* EDIT to edituser.
+*/
+router.put('/edituser/:id',function(req,res){
+  var db = req.db;
+  var userToUpdate = req.params.id;
+  db.collection('userlist').findById(userToUpdate,function(err,p){
+    if(!p){
+      return next(new Error('Could not load Document'));
+    } else {
+      //update the file
+      p.username = req.body.username;
+      p.username = req.body.username;
+      p.username = req.body.username;
+
+      p.save;
+    }
+    
+  });
+});
+
+module.exports = router;
